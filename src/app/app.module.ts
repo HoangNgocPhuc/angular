8366c7453745baef2407c74adabe9aa6c6adb56a
HNP <phuc@gmail.com>
2022-04-21 22:50:32 +0900

    git init

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
+
+@NgModule({
+  declarations: [
+    // component that is created whenever a new project is initiated 
+    AppComponent,
+    NewCmpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  // reference to the services created.
+  providers: [],
+  // reference to the default component created
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
