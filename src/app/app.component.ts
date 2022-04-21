29c8e62db4e051aea740c7f9e31fc481313f6468
HNP <phuc@gmail.com>
2022-04-21 23:39:15 +0900

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,13 @@ export class AppComponent {
   months = ["January", "February", "March", "April", "May", "June", "July", 
       "August", "September", "October", "November", "December"];
   isavailable = false;
+  myClickFunction(event: any) {
+    //just added console.log which will display the event details in browser on click of the button.
+    alert("Button is clicked");
+    console.log(event);
+ }
+  changemonths(event: any) {
+    console.log("Changed month from the Dropdown");
+    console.log(event);
+  }
 }
